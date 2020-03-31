0e9a76e0d03aa79a2faab1d6dda2fe06ff572edf
vpolchuk <vpolchuk@playtika.com>
2020-03-31 17:40:41 +0300

    Fix for passing aerospike timeout parameters

--- aerospike-storage-backend/src/main/java/com/playtika/janusgraph/aerospike/AerospikePolicyProvider.java
+++ aerospike-storage-backend/src/main/java/com/playtika/janusgraph/aerospike/AerospikePolicyProvider.java
@@ -1,35 +1,81 @@
 package com.playtika.janusgraph.aerospike;
 
+import com.aerospike.client.policy.BatchPolicy;
+import com.aerospike.client.policy.ClientPolicy;
 import com.aerospike.client.policy.Policy;
+import com.aerospike.client.policy.QueryPolicy;
 import com.aerospike.client.policy.ScanPolicy;
 import com.aerospike.client.policy.WritePolicy;
+import org.janusgraph.diskstorage.configuration.Configuration;
+
+import static com.playtika.janusgraph.aerospike.ConfigOptions.AEROSPIKE_CONNECTIONS_PER_NODE;
+import static com.playtika.janusgraph.aerospike.ConfigOptions.AEROSPIKE_READ_TIMEOUT;
+import static com.playtika.janusgraph.aerospike.ConfigOptions.AEROSPIKE_WRITE_TIMEOUT;
+import static com.playtika.janusgraph.aerospike.ConfigOptions.TEST_ENVIRONMENT;
+import static org.janusgraph.graphdb.configuration.GraphDatabaseConfiguration.AUTH_PASSWORD;
+import static org.janusgraph.graphdb.configuration.GraphDatabaseConfiguration.AUTH_USERNAME;
 
 public class AerospikePolicyProvider {
 
-    public WritePolicy writePolicy(){
+    private Configuration configuration;
+
+    public AerospikePolicyProvider(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    public ClientPolicy clientPolicy() {
+        ClientPolicy clientPolicy = new ClientPolicy();
+        clientPolicy.user = configuration.has(AUTH_USERNAME) ? configuration.get(AUTH_USERNAME) : null;
+        clientPolicy.password = configuration.has(AUTH_PASSWORD) ? configuration.get(AUTH_PASSWORD) : null;
+        clientPolicy.maxConnsPerNode = configuration.get(AEROSPIKE_CONNECTIONS_PER_NODE);
+        clientPolicy.readPolicyDefault = readPolicy();
+        clientPolicy.scanPolicyDefault = scanPolicy();
+        clientPolicy.queryPolicyDefault = queryPolicy();
+        clientPolicy.writePolicyDefault = writePolicy();
+        clientPolicy.batchPolicyDefault = batchPolicy();
+        return clientPolicy;
+    }
+
+    public BatchPolicy batchPolicy() {
+        BatchPolicy batchPolicy = new BatchPolicy();
+        batchPolicy.totalTimeout = configuration.get(AEROSPIKE_WRITE_TIMEOUT);
+        return batchPolicy;
+    }
+
+    public QueryPolicy queryPolicy() {
+        QueryPolicy queryPolicy = new QueryPolicy();
+        queryPolicy.totalTimeout = configuration.get(AEROSPIKE_READ_TIMEOUT);
+        return queryPolicy;
+    }
+
+    public WritePolicy writePolicy() {
         WritePolicy writePolicy = new WritePolicy();
         writePolicy.sendKey = true;
         writePolicy.expiration = -1;
+        writePolicy.totalTimeout = configuration.get(AEROSPIKE_WRITE_TIMEOUT);
         return writePolicy;
     }
 
-    public WritePolicy deletePolicy(){
+    public WritePolicy deletePolicy() {
         WritePolicy deletePolicy = new WritePolicy();
         deletePolicy.expiration = -1;
-        deletePolicy.durableDelete = true;
+        deletePolicy.totalTimeout = configuration.get(AEROSPIKE_WRITE_TIMEOUT);
+        deletePolicy.durableDelete = !configuration.get(TEST_ENVIRONMENT);
         return deletePolicy;
     }
 
-    public Policy readPolicy(){
+    public Policy readPolicy() {
         Policy readPolicy = new Policy();
         readPolicy.sendKey = true;
+        readPolicy.totalTimeout = configuration.get(AEROSPIKE_READ_TIMEOUT);
         return readPolicy;
     }
 
-    public ScanPolicy scanPolicy(){
+    public ScanPolicy scanPolicy() {
         ScanPolicy scanPolicy = new ScanPolicy();
         scanPolicy.sendKey = true;
         scanPolicy.includeBinData = true;
+        scanPolicy.totalTimeout = configuration.get(AEROSPIKE_READ_TIMEOUT);
         return scanPolicy;
     }
 }
