3cb3619129da6c2f74d1edaaa92e0f6d7b4a1955
Sergii Karpenko <skarpenko@playtika.com>
2021-08-19 14:35:34 +0300

    trying to fix ghost vertices - order updates [indexes deletion, edge deletion, edge creation, index creation]

--- aerospike-storage-backend/src/main/java/com/playtika/janusgraph/aerospike/operations/batch/BatchUpdateOperations.java
+++ aerospike-storage-backend/src/main/java/com/playtika/janusgraph/aerospike/operations/batch/BatchUpdateOperations.java
@@ -3,7 +3,11 @@ package com.playtika.janusgraph.aerospike.operations.batch;
 import com.playtika.janusgraph.aerospike.operations.BasicMutateOperations;
 import nosql.batch.update.UpdateOperations;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.playtika.janusgraph.aerospike.util.AsyncUtil.completeAll;
 
@@ -19,7 +23,8 @@ public class BatchUpdateOperations implements UpdateOperations<BatchUpdates> {
 
     @Override
     public void updateMany(BatchUpdates batchUpdates, boolean calledByWal) {
-        completeAll(batchUpdates.getUpdates(),
+        BatchUpdates.UpdatesOrdered updatesOrdered = batchUpdates.getUpdates();
+        completeAll(concat(updatesOrdered.indexDeletion, updatesOrdered.edgeCreation),
                 updateValue -> true,
                 updateValue -> {
                     mutateOperations.mutate(
@@ -28,5 +33,38 @@ public class BatchUpdateOperations implements UpdateOperations<BatchUpdates> {
                 },
                 () -> null,
                 executorService);
+
+        completeAll(concat(updatesOrdered.indexCreation, updatesOrdered.edgeDeletion),
+                updateValue -> true,
+                updateValue -> {
+                    mutateOperations.mutate(
+                            updateValue.storeName, updateValue.key, updateValue.values);
+                    return true;
+                },
+                () -> null,
+                executorService);
+
+        completeAll(updatesOrdered.other,
+                updateValue -> true,
+                updateValue -> {
+                    mutateOperations.mutate(
+                            updateValue.storeName, updateValue.key, updateValue.values);
+                    return true;
+                },
+                () -> null,
+                executorService);
+   }
+
+   private static <V> List<V> concat(List<V> list1, List<V> list2){
+        if(list1.isEmpty()){
+            return list2;
+        } else if(list2.isEmpty()){
+            return list1;
+        } else {
+            List<V> result = new ArrayList<>(list1.size() + list2.size());
+            result.addAll(list1);
+            result.addAll(list2);
+            return result;
+        }
    }
 }
--- aerospike-storage-backend/src/main/java/com/playtika/janusgraph/aerospike/operations/batch/BatchUpdates.java
+++ aerospike-storage-backend/src/main/java/com/playtika/janusgraph/aerospike/operations/batch/BatchUpdates.java
@@ -2,25 +2,131 @@ package com.playtika.janusgraph.aerospike.operations.batch;
 
 import com.aerospike.client.Value;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class BatchUpdates {
 
-    private final List<UpdateValue> updates;
-    private Map<String, Map<Value, Map<Value, Value>>> updatesByStore;
+    public static final String INDEX_STORE_NAME = "graphindex";
+    public static final String EDGE_STORE_NAME = "edgestore";
+    private final UpdatesOrdered updatesOrdered;
+    private final Map<String, Map<Value, Map<Value, Value>>> updatesByStore;
 
     public BatchUpdates(Map<String, Map<Value, Map<Value, Value>>> updatesByStore){
-        updates = updatesByStore.entrySet().stream()
-                .flatMap(storeEntry -> storeEntry.getValue().entrySet().stream()
-                        .map(keyEntry -> new UpdateValue(storeEntry.getKey(), keyEntry.getKey(), keyEntry.getValue())))
-                .collect(Collectors.toList());
+        this.updatesOrdered = orderUpdates(updatesByStore);
         this.updatesByStore = updatesByStore;
     }
 
-    public List<UpdateValue> getUpdates() {
-        return updates;
+    /**
+     * Order updates [indexes deletion, edge deletion, edge creation, index creation]
+     * @param updatesByStore
+     * @return
+     */
+    private static UpdatesOrdered orderUpdates(Map<String, Map<Value, Map<Value, Value>>> updatesByStore){
+        List<UpdateValue> indexDeletion = new ArrayList<>();
+        List<UpdateValue> edgeDeletion = new ArrayList<>();
+        List<UpdateValue> edgeCreation = new ArrayList<>();
+        List<UpdateValue> indexCreation = new ArrayList<>();
+        List<UpdateValue> other = new ArrayList<>();
+
+        for(Map.Entry<String, Map<Value, Map<Value, Value>>> updatesByStoreEntry : updatesByStore.entrySet()){
+            String storeName = updatesByStoreEntry.getKey();
+            for(Map.Entry<Value, Map<Value, Value>> keyUpdate : updatesByStoreEntry.getValue().entrySet()){
+                Value key = keyUpdate.getKey();
+
+                switch (storeName) {
+                    case INDEX_STORE_NAME: {
+                        Split<Map<Value, Value>> split = splitToCreationAndDeletion(keyUpdate.getValue());
+                        if (split.created != null) {
+                            indexCreation.add(new UpdateValue(storeName, key, split.created));
+                        }
+                        if (split.deleted != null) {
+                            indexDeletion.add(new UpdateValue(storeName, key, split.deleted));
+                        }
+                        break;
+                    }
+                    case EDGE_STORE_NAME: {
+                        Split<Map<Value, Value>> split = splitToCreationAndDeletion(keyUpdate.getValue());
+                        if (split.created != null) {
+                            edgeCreation.add(new UpdateValue(storeName, key, split.created));
+                        }
+                        if (split.deleted != null) {
+                            edgeDeletion.add(new UpdateValue(storeName, key, split.deleted));
+                        }
+                        break;
+                    }
+                    default:
+                        other.add(new UpdateValue(storeName, key, keyUpdate.getValue()));
+                }
+            }
+        }
+        return new UpdatesOrdered(indexDeletion, edgeDeletion, edgeCreation, indexCreation, other);
+    }
+
+    static class UpdatesOrdered {
+        final List<UpdateValue> indexDeletion;
+        final List<UpdateValue> edgeDeletion;
+        final List<UpdateValue> edgeCreation;
+        final List<UpdateValue> indexCreation;
+        final List<UpdateValue> other;
+
+        UpdatesOrdered(List<UpdateValue> indexDeletion,
+                       List<UpdateValue> edgeDeletion,
+                       List<UpdateValue> edgeCreation,
+                       List<UpdateValue> indexCreation,
+                       List<UpdateValue> other) {
+            this.indexDeletion = indexDeletion;
+            this.edgeDeletion = edgeDeletion;
+            this.edgeCreation = edgeCreation;
+            this.indexCreation = indexCreation;
+            this.other = other;
+        }
+    }
+
+    private static Split<Map<Value, Value>> splitToCreationAndDeletion(Map<Value, Value> updates){
+        boolean onlyCreation = true;
+        boolean onlyDeletion = true;
+        for(Map.Entry<Value, Value> updateEntry : updates.entrySet()){
+            if(updateEntry.getValue() == null){
+                onlyCreation = false;
+            } else {
+                onlyDeletion = false;
+            }
+        }
+
+        if(onlyCreation){
+            return new Split<>(updates, null);
+        } else if(onlyDeletion){
+            return new Split<>(null, updates);
+        } else {
+            Map<Value, Value> creation = new HashMap<>(updates.size());
+            Map<Value, Value> deletion = new HashMap<>(updates.size());
+            for(Map.Entry<Value, Value> updateEntry : updates.entrySet()){
+                if(updateEntry.getValue() == null){
+                    deletion.put(updateEntry.getKey(), null);
+                } else {
+                    deletion.put(updateEntry.getKey(), updateEntry.getValue());
+                }
+            }
+            return new Split<>(creation, deletion);
+        }
+    }
+
+    private static class Split<V>{
+
+        final V created;
+        final V deleted;
+
+        public Split(V created, V deleted) {
+            this.created = created;
+            this.deleted = deleted;
+        }
+    }
+
+    public UpdatesOrdered getUpdates() {
+        return updatesOrdered;
     }
 
     public Map<String, Map<Value, Map<Value, Value>>> getUpdatesByStore() {
