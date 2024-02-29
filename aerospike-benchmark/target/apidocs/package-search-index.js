c50051980761d6ef158cfa4ae23cac8b2d8126e4
skarpenko <skarpenko@playtika.com>
2024-02-29 22:15:15 +0200

    Remove files from target

--- aerospike-benchmark/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.playtika.janusgraph.aerospike.benchmark"},{"l":"com.playtika.janusgraph.aerospike.benchmark.jmh_generated"}];updateSearchResults();
--- aerospike-benchmark/target/generated-sources/annotations/com/playtika/janusgraph/aerospike/benchmark/jmh_generated/JanusgraphBenchmarks_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.playtika.janusgraph.aerospike.benchmark.jmh_generated;
-public class JanusgraphBenchmarks_jmhType extends JanusgraphBenchmarks_jmhType_B3 {
-}
-
